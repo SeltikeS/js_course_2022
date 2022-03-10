2346d5233562ab3562e7db858c5999f02dff474b
SeltikeS <pschenka@gmail.com>
2022-03-10 23:03:11 +0300

    lab4: add new functions gettweet, gettweets, changeuser.

--- ui/twits.js
+++ ui/twits.js
@@ -1,8 +1,73 @@
-;(function() {
+;const tweetModul = (function() {
 
+    // Prototypes and variables
+    let user = 'SeltikeS';
+    const tweets = [];  
+
+
+    const tweet = { id: 0 };
+    const commment = { id: 0 };
+    
 
 
     // Tweet functions
+
+    function getTweets(skip = 0, top = 10, filterConfig = {
+        author: null,
+        dateFrom: null,
+        dateTo: new Date(),
+        hashtags: null,
+        text: ""
+    }) {
+        const out = [];
+        let skipCnt = skip;
+        let topCnt = top;
+
+        function filterConfigCheck(filt, tw) {
+
+            if((filt.author) && (filt.author !== tw.author)) {
+                return false;
+            }
+            if((filt.dateTo < tw.createdAt) || (filt.dateFrom > tw.createdAt)) {
+                return false;
+            }            
+            if((filt.hashtags) && (!tw.text.includes(filt.hashtags))) {
+                return false;
+            }
+            if((filt.text) && (!tw.text.includes(filt.text))) {
+                return false;
+            }
+
+            return true;
+        }
+
+        for(let i = 0; i < tweets.length; ++i) {
+            if(skipCnt) {
+                skipCnt--;
+                continue;
+            }
+
+            if(topCnt) {
+                if(filterConfigCheck(filterConfig, tweets[i])) {
+                    out.push(tweets[i]);
+                }
+                topCnt--;
+            } else {
+                break;
+            }
+        }
+
+        return out;
+    }
+
+    function getTweet(id) {
+        for(let i = 0; i < tweets.length; ++i) {
+            if(tweets[i].id === id) {
+                return tweets[i];
+            }
+        }
+        return undefined;
+    }  
 
     function addTweet(newText) {
         tweets.push(Object.create(tweet, {
@@ -19,33 +84,44 @@
                 value: new Date(),
                 enumerable: true,
             },
-            autor: {
+            author: {
                 value: String(user),
                 enumerable: true
             },
             comments: {
-                value: new Array(),
+                value: [],
                 enumerable: true,
                 writable: true
             }
         }));
     }
 
+    function changeUser(usr) {
+        user = usr;
+    }
+
     // Add tweets
 
-    addTweet('Write text');
-    addTweet('Write text');
+
+    for(let i = 0; i < 20; ++i) {
+        addTweet('Write text');
+    }
+
+    addTweet('A bc d #js #datamola');
+
+
+    
 
     console.log(tweets);
-    
+    console.log(getTweets(3, 6));
 
-    // Prototypes and Variables
+    console.log(getTweets(0, 30, {hashtags: '#js'}));
 
-    const tweets = new Array();
-    
-    const tweet = new Object({ id: 0 });
-    const commment = new Object({ id: 0 });
-    const user = 'SeltikeS';
-    
 
+        
 }());
+
+
+
+
+
