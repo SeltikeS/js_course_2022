5e411db0055e8d2dfe71494de8f5f2b260626ead
SeltikeS <pschenka@gmail.com>
2022-03-11 17:54:08 +0300

    Lab4extra: 1st task fix

--- exra-tasks/lab4-1/index.js
+++ exra-tasks/lab4-1/index.js
@@ -2,9 +2,8 @@ const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 let sum = arr[0];
 
 const nArr = arr;
-console.log(nArr);
 
-for(let i = 0; i < arr.length; ++i) {
+arr.forEach(item => {
     nArr.reduce((total, item) => {
         let nSum = total + item;
         if(nSum > sum) {
@@ -13,6 +12,6 @@ for(let i = 0; i < arr.length; ++i) {
         return nSum;
     }, 0)
     nArr.shift();
-}
+})
 
 console.log(sum);
