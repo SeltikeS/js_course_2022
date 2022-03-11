d9645ac3859037a9fb29711d458ed7b32f8711b4
SeltikeS <pschenka@gmail.com>
2022-03-11 15:33:16 +0300

    Lab4: Add all functions from main task.

--- ui/twits.js
+++ ui/twits.js
@@ -6,7 +6,7 @@
 
 
     const tweet = { id: 0 };
-    const commment = { id: 0 };
+    const comment = { id: 0 };
     
 
 
@@ -69,8 +69,19 @@
         return undefined;
     }  
 
+    function validateTweet(tw) {
+        if(typeof(tw.id) === 'string' && 
+           typeof(tw.text) === 'string' && 
+           typeof(tw.createdAt) === 'object' && 
+           typeof(tw.author) === 'string' && 
+           typeof(tw.comments) === 'object') {
+            return true;
+        }
+        return false;
+    }
+
     function addTweet(newText) {
-        tweets.push(Object.create(tweet, {
+        const newTweet = Object.create(tweet, {
             id: {
                 value: String(++tweet.id),
                 enumerable: true
@@ -93,14 +104,81 @@
                 enumerable: true,
                 writable: true
             }
-        }));
+        });
+
+        if(validateTweet(newTweet)) { 
+            tweets.push(newTweet);
+            return true;
+        }
+        return false;
+    }
+
+    function editTweet(id, text) {
+        const tw = getTweet(id);
+        if(validateTweet(tw) && tw.author === user) {
+            tw.text = text;
+            return true;
+        }
+        return false;
+    }
+
+    function removeTweet(id) {  
+        for(let i = 0; i < tweets.length; ++i) {
+            if(tweets[i].id === id) {
+                tweets.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function validateComment(com) {
+        if(typeof(com.id) === 'string' && 
+           typeof(com.text) === 'string' && 
+           typeof(com.createdAt) === 'object' && 
+           typeof(com.author) === 'string') {
+            return true;
+        }
+        return false;
+    }
+
+    function addComment(id, text) {
+        const newComment = Object.create(comment, {
+            id: {
+                value: String(++comment.id),
+                enumerable: true
+            },
+            text: {
+                value: String(text),
+                enumerable: true,
+                writable: true
+            },
+            createdAt: {
+                value: new Date(),
+                enumerable: true,
+            },
+            author: {
+                value: String(user),
+                enumerable: true
+            }
+        })
+
+        if(validateComment(newComment)) {
+            for(let i = 0; i < tweets.length; ++i) {
+                if(validateTweet(tweets[i]) && tweets[i].id === id) {
+                    tweets[i].comments.push(newComment);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     function changeUser(usr) {
         user = usr;
     }
 
-    // Add tweets
+    // Check results
 
 
     for(let i = 0; i < 20; ++i) {
@@ -109,16 +187,35 @@
 
     addTweet('A bc d #js #datamola');
 
-
-    
-
+    // Check addTweet and getTweets
     console.log(tweets);
     console.log(getTweets(3, 6));
 
+    // Check filterConfig
+    console.log(getTweets(0, 30, {author: 'SeltikeS'}));
+    console.log(getTweets(0, 30, {text: 't'}));
     console.log(getTweets(0, 30, {hashtags: '#js'}));
+    console.log(getTweets(0, 30, {dateFrom: null}));
+    console.log(getTweets(0, 30, {dateTo: new Date()}));
 
+    // Check validateTweet
+    console.log(validateTweet(tweets[0]));
 
-        
+    // Check addTweet
+    console.log(addTweet('Write text'));
+
+    // Check editTweet
+    console.log(editTweet('22', 'New text'));
+    console.log(tweets);
+
+    // Check removeTweet
+    console.log(removeTweet('22'));
+    console.log(tweets);
+
+    // Check addComment
+    console.log(addComment('1', 'new comment'));
+    console.log(tweets);
+
 }());
 
 
