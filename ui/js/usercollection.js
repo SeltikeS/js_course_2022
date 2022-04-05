12d1b69c00b1ca3ab9ebfbc96d3c93259881e0b0
SeltikeS <pschenka@gmail.com>
2022-04-05 13:02:19 +0300

    lab7: create tweets controller. push old functions to controller

---
+++ ui/js/usercollection.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+
+class UserCollection {
+  constructor() {
+    
+  }
+}
